ef6b9ecd99f18fe8c3f63e1167604f2eea6f7188
Mikhail Belokhvostov <mikbel13@gmail.com>
2021-06-23 21:46:05 +0300

    п.п. 1-6: Создана группа «Profile», в ней файлы ProfileHeaderView.swift и ProfileViewController.swift. Изменен в коде цвет ProfileViewController на серый.

---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Mikhail Belokhvostov on 23.06.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        let view = Bundle.main.loadNibNamed("GGView", owner: nil, options: nil)?.first as! UIView
+//        view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//// //       let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.backgroundColor = .lightGray
+//        self.view.addSubview(view)
+    }
+}
