b52097d168ffb0f4df01446c62712c3bafc8bfd5
Mikhail Belokhvostov <mikbel13@gmail.com>
2021-07-07 22:16:38 +0300

    Исправлено Домашнее задание к занятию "2.2 Верстка экранов для iOS. Auto Layout"

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -15,24 +15,19 @@ class ProfileViewController: UIViewController {
         bottomButton.setTitle("Нижняя кнопка", for: .normal)
         bottomButton.backgroundColor = .black
         return bottomButton
-
     }()
 
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         view.addSubview(bottomButton)
         bottomButton.translatesAutoresizingMaskIntoConstraints = false
-
+        
         NSLayoutConstraint.activate([
         bottomButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
         bottomButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            bottomButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            bottomButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,  constant: -10)
+        bottomButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+        bottomButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,  constant: -10)
         ])
     }
-    
-    
-    
-    
+
 }
